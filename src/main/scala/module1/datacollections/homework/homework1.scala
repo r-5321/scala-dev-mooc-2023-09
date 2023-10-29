3d2ae6aac4cca3570dd005cfed3815967a10c29a
RIskenderov <RIskenderov>
2023-10-29 16:33:42 +0300

    hw 2

--- src/main/scala/module1/datacollections/homework/homework1.scala
+++ src/main/scala/module1/datacollections/homework/homework1.scala
@@ -1,20 +1,28 @@
 package module1.datacollections.homework
 
 import scala.util.Random
-/*
+
 class BallsExperiment {
 
+  def shaker(list: List[Int]): List[Int] = Random.shuffle(list)
+
+
+  val UrnList: List[Int] = shaker(List.fill(3)(1) ++ List.fill(3)(0))
+
+
   def isFirstBlackSecondWhite(): Boolean = {
-    ???
+    val firstBall = UrnList.head
+    val secondBall = shaker(UrnList.tail)
+    if (firstBall == 1 || secondBall == 1) true else false
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
     val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments = listOfExperiments.map(e => e.isFirstBlackSecondWhite())
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
